e4c4513a83b341d6cd93ff58d96f33636aa68872
Jonny Yang <jiamu@outlook.com>
2022-05-28 23:26:36 +0100

    finished Find PI to the Nth Digit
    I decided to use a PI string because I was lazy

--- src/Find PI to the Nth Digit/main.cpp
+++ src/Find PI to the Nth Digit/main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <math.h>
+#include <string>
 
 //Enter a number and have the program generate PI up to that many decimal places. Keep a limit to how far the program will go.
 
 int main(void)
 {
 	int digits;
-	double pi = atan(1)*4;
+	std::string pi = "3.141592653589793238462643383279502884197169399375105820974944592307816";
 	std::cout << "Enter desired PI digit amount, with a limit of 69" << std::endl;
 	std::cin >> digits;
 	if (digits > 0 && digits < 70)
 	{
-		std::cout << std::to_string(digits)
+		if(digits > 1) digits++;
+		std::cout << pi.substr(0, digits);
 	}
 	return 0;
 }
